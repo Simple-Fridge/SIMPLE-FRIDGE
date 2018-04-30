fdaefb386f0cb10bd5808def702f533b672bed83
kabdullah9 <kabdullah99@gmail.com>
2018-04-30 12:17:31 -0700

    update

--- controllers/usdaApi.js
+++ controllers/usdaApi.js
@@ -3,29 +3,32 @@ var request = require('request');
 function foodSearch(query, callback) {
   var data = 'no results';
   var url = 'https://api.nal.usda.gov/ndb/search/?format=json&q=' + query + '&ds=Standard Reference&max=1&api_key=WaKpXBFRRj4A6YcAcgNCi9DzIi2HSHTi2tqyyHcZ';
-  request(url, function(error, response, body) {
+  ; console.log(url)
+  request(url, function (error, response, body) {
     if (error) throw error;
     var results = JSON.parse(body);
     var foodItem = results.list.item[0].ndbno;
+    console.log(foodItem);
     callback(foodItem);
   });
 }
 
-function nutrientSearch(ndbno, callback){
+function nutrientSearch(ndbno, callback) {
   var url = 'https://api.nal.usda.gov/ndb/V2/reports?ndbno=' + ndbno + '&type=f&format=json&api_key=WaKpXBFRRj4A6YcAcgNCi9DzIi2HSHTi2tqyyHcZ'
-  request(url, function(error, response, body){
+  request(url, function (error, response, body) {
     if (error) throw error;
     var stats = []
     body = JSON.parse(body)
     var nutrients = body.foods[0].food.nutrients
-    for (var i = 0 ; i < nutrients.length; i++){
+    for (var i = 0; i < nutrients.length; i++) {
       var nutriName = nutrients[i].name
       var nutriValue = nutrients[i].value
       var nutriUnit = nutrients[i].unit
-      if(nutriName === 'Protein' || nutriName === 'Carbohydrate, by difference' || nutriName === 'Fiber, total dietary' || nutriName === 'Sugars, total' || nutriName === 'Cholesterol' || nutriName === 'Total lipid (fat)' ){
-        stats.push(nutriName + ' ' +nutriValue + nutriUnit )
+      if (nutriName === 'Protein' || nutriName === 'Carbohydrate, by difference' || nutriName === 'Fiber, total dietary' || nutriName === 'Sugars, total' || nutriName === 'Cholesterol' || nutriName === 'Total lipid (fat)') {
+        nutrients[i].formattedName = nutriName + ' ' + nutriValue + nutriUnit;
+        stats.push(nutrients[i]);
         // console.log(nutriName + ' ' +nutriUnit + ' ' + nutriValue)      
-      }  
+      }
     }
     callback(stats);
   });
@@ -34,8 +37,8 @@ function nutrientSearch(ndbno, callback){
 }
 
 function findNutrientsByFoodName(foodName, callback) {
-  foodSearch(foodName, function(nbdno){
-    nutrientSearch(nbdno, function(results){
+  foodSearch(foodName, function (nbdno) {
+    nutrientSearch(nbdno, function (results) {
       callback(results);
     });
   });
