0c2b24ce14a2ea8b517504db36096099146dcbf1
kabdullah9 <kabdullah99@gmail.com>
2018-04-30 10:24:47 -0700

    update

---
+++ controllers/usdaApi.js
@@ -0,0 +1,48 @@
+var request = require('request');
+
+function foodSearch(query, callback) {
+  var data = 'no results';
+  var url = 'https://api.nal.usda.gov/ndb/search/?format=json&q=' + query + '&ds=Standard Reference&max=1&api_key=WaKpXBFRRj4A6YcAcgNCi9DzIi2HSHTi2tqyyHcZ';
+  request(url, function(error, response, body) {
+    if (error) throw error;
+    var results = JSON.parse(body);
+    var foodItem = results.list.item[0].ndbno;
+    callback(foodItem);
+  });
+}
+
+function nutrientSearch(ndbno, callback){
+  var url = 'https://api.nal.usda.gov/ndb/V2/reports?ndbno=' + ndbno + '&type=f&format=json&api_key=WaKpXBFRRj4A6YcAcgNCi9DzIi2HSHTi2tqyyHcZ'
+  request(url, function(error, response, body){
+    if (error) throw error;
+    var stats = []
+    body = JSON.parse(body)
+    var nutrients = body.foods[0].food.nutrients
+    for (var i = 0 ; i < nutrients.length; i++){
+      var nutriName = nutrients[i].name
+      var nutriValue = nutrients[i].value
+      var nutriUnit = nutrients[i].unit
+      if(nutriName === 'Protein' || nutriName === 'Carbohydrate, by difference' || nutriName === 'Fiber, total dietary' || nutriName === 'Sugars, total' || nutriName === 'Cholesterol' || nutriName === 'Total lipid (fat)' ){
+        stats.push(nutriName + ' ' +nutriValue + nutriUnit )
+        // console.log(nutriName + ' ' +nutriUnit + ' ' + nutriValue)      
+      }  
+    }
+    callback(stats);
+  });
+
+
+}
+
+function findNutrientsByFoodName(foodName, callback) {
+  foodSearch(foodName, function(nbdno){
+    nutrientSearch(nbdno, function(results){
+      callback(results);
+    });
+  });
+}
+
+module.exports = {
+  foodSearch: foodSearch,
+  nutrientSearch: nutrientSearch,
+  findNutrientsByFoodName: findNutrientsByFoodName
+};
---
+++ public/assets/js/index.js
@@ -0,0 +1,4 @@
+// food name will reture nutrients 
+// get the food name
+// send request to server
+// display the results
