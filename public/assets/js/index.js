cbcb626237e084b35da396bc8e9c3db2010b9de0
kabdullah9 <kabdullah99@gmail.com>
2018-04-30 14:12:16 -0700

    end of day update

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,3 +2,14 @@
 // get the food name
 // send request to server
 // display the results
+function displaySearchResult(search) {
+    window.location.replace("?=" + food +"");
+}
+
+$('#submit').on('click', function (){
+    // get text from thesearch box
+
+    var searchText = $('#searchText').text().trim();
+
+    displaySearchResult(searchText);
+})
