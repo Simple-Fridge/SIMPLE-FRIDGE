df55fa9d120fa047382f33a4a61befcaa7685e9d
erick016 <erick016@cougars.csusm.edu>
2018-04-30 11:27:16 -0700

    Only Users table works so far!

--- db/nutrition_db.sql
+++ db/nutrition_db.sql
@@ -7,3 +7,19 @@ USE nutrition_db;
 SELECT * FROM nutrients; 
 
 SELECT * FROM foods; 
+
+DROP TABLE if exists users;
+
+CREATE TABLE users
+(
+	id int NOT NULL AUTO_INCREMENT,
+	email varchar(255) NOT NULL,
+	myPassword bigint,
+	passwordEnc bigint NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE Trigger encrypt BEFORE INSERT ON users for each Row set New.passwordEnc = MOD(POW(New.myPassword,9),106), New.myPassword = 0;
+
+INSERT INTO users (email,myPassword,passwordEnc)
+VALUES ("snoopy@snoopy.com", 66, 0);
